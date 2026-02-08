5ec5f2b87a2fe0176683d4fbb40f3be94f5f3955
HassanAsh34 <126121195+HassanAsh34@users.noreply.github.com>
2026-02-08 04:50:24 +0200

    fixed list students of teacher

--- grad/Services/TeacherServices.cs
+++ grad/Services/TeacherServices.cs
@@ -19,19 +19,58 @@ namespace grad.Services
 			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
 			_lessonServices = lessonServices ?? throw new ArgumentNullException(nameof(lessonServices));
 		}
-		public async Task<ResultDTO> ShowStudents(Guid Sid,CancellationToken cancellation)
+
+		public async Task<ResultDTO> ShowStudents(Guid Sid, CancellationToken cancellation)
 		{
-			if (await _subjectServices.IsSubjectExist(subjectId: Sid))
-			{
-				IEnumerable<Enrollement> enrollement = await _repository.GetEntitiesAsync<Enrollement>(e => e.SUBFK == Sid,cancellationToken: cancellation);
+			if (!await _subjectServices.IsSubjectExist(subjectId: Sid))
 				return new ResultDTO
 				{
-					StatusCode = StatusCodes.Status200OK,
-					result = enrollement
+					StatusCode = StatusCodes.Status500InternalServerError,
+					Message = "Failed to get Students"
+				};
+			else 
+			{
+				var students = await _repository
+					.GetEntitiesAsync<Enrollement>(
+						e => e.SUBFK == Sid,
+						q => q.Include(e=>e.Student).Include(e=>e.Student.parent),
+						cancellationToken: cancellation
+					);
+				var result = students.Select(e => new LISTStudentDTO
+				{
+					Id = e.Student.Id,
+					name = $"{e.Student.FName} {e.Student.LName}",
+					email = e.Student.EmailorUserName,
+					parentInfo = e.Student.parent != null ? new parentInfo
+					{
+						Email = e.Student.parent.EmailorUserName,
+						phoneNumber = e.Student.parent.phoneNumber
+					} : null
+				}).ToList();
+
+
+				//IEnumerable<LISTStudentDTO> result = students.Select(e => new LISTStudentDTO
+				//{
+				//	Id = e.Student.Id,
+				//	name = $"{e.Student.FName} {e.Student.LName}",
+				//	email = e.Student.EmailorUserName,
+				//	parentInfo = e.Student.parent != null ? new parentInfo
+				//	{
+				//		Email = e.Student.parent.EmailorUserName,
+				//		phoneNumber = e.Student.parent.phoneNumber
+				//	} : null
+				//	//add his progress
+				//	// add only what you need
+				//});
+				int stdCount = result.Count;
+
+				return new ResultDTO
+				{
+					Message = $"{stdCount} students were found",
+					StatusCode = stdCount >0 ? StatusCodes.Status200OK : StatusCodes.Status204NoContent,
+					result = result
 				};
 			}
-			else
-				throw new NotImplementedException();
 
 		}
 
